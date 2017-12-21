af04ae912411dfc4370b861d4fd8f1d4556b2da1
Anders Jürisoo <jurisoo@hotmail.com>
2017-12-21 18:18:35 +0100

    changed name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel Stimpack</title>
+        <title>Laravel/React/Redux</title>
     </head>
     <body>        
         <div id="app"></div>
